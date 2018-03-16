906fdd3e62820fadfef856e7687ef3d2a3f84cae
Walker <1424234500@qq.com>
2018-03-16 17:52:10 +0800

    Fri Mar 16 17:52:10 DST 2018

---
+++ src/util/SingletonObject.java
@@ -0,0 +1,15 @@
+
+// Warning: No line numbers available in class file
+package util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SingletonObject {
+}
