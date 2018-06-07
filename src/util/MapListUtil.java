4373ebed5cb2ecd40e6906f6a2e9a78f00b315a8
Walker <1424234500@qq.com>
2018-06-07 18:21:32 +0800

    Thu Jun  7 18:21:31 DST 2018

--- src/util/MapListUtil.java
+++ src/util/MapListUtil.java
@@ -1,5 +1,7 @@
 package util;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
- 
+
 
 /**
  * map list常用工具
@@ -21,34 +23,34 @@ public class MapListUtil {
 	public static void main(String[] argc){
 		testListSetMap();
 	}
-	
+
 	/**
 	 *  测试list set map相关区别 争议点
 	 */
 	public static void testListSetMap(){
 		Tools.out("-------测试list set map相关区别 争议点");
-		
+
 		ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		
+
 		Map map = getMap().put("key1", "value1").put(null, "value null").put("value null", null).build();
 		Map map2 = getMap().put("key2", "value2").build();
-		  
+
 		Map map22 = getMap().put("key2", "value2").build();
 		Map map3 = map;
 		Tools.out("map.keySet 键集合",map.keySet());
 		Tools.out("map.entrySet 键值集合",map.entrySet());
-		//for (Map.Entry<String, Object> entry : map.entrySet()) 
-		
-		list.add(map); 
-		list.add(map2); 
+		//for (Map.Entry<String, Object> entry : map.entrySet())
+
+		list.add(map);
+		list.add(map2);
 		Tools.out("hash map键值可null 无序",list);
 		Tools.out("map==map", map==map, "map.equals(map)", map.equals(map),
 				"map==map2", map==map2, "map.equals(map2)", map.equals(map2),
 				"map==map3", map==map3, "map.equals(map3)", map.equals(map3),
 				"map2==map22", map2==map22, "map2.equals(map22) equals有效值判断=", map2.equals(map22)
-				);
-		list.add(map); 
-		list.add(map3); 
+		);
+		list.add(map);
+		list.add(map3);
 		Tools.out(list);
 
 		HashSet<Map> set = new HashSet<Map>();
@@ -59,47 +61,47 @@ public class MapListUtil {
 		set.add(map3);
 		set.add(map22);
 		Tools.out("set 相同引用map equals判断不重复", set);
-		 
+
 		HashMap<String, Object> hmap = new HashMap<>();
 		hmap.put(null, "valueNull");
 		hmap.put("keynull", null);
 		hmap.put("key1", "value1");
-		
-		Tools.out("hash", hmap); 
- 
+
+		Tools.out("hash", hmap);
+
 
 		Hashtable<String, Object> htable = new Hashtable<>();
 //		htable.put(null, "valueNull");
 //		htable.put("keynull", null);
 		htable.put("key1", "value1");
-		
-		Tools.out("table 键值", htable); 
-		
-		Tools.out("-----------"); 
-		
+
+		Tools.out("table 键值", htable);
+
+		Tools.out("-----------");
+
 		List<Map<String, Object>> li = new ArrayList<Map<String, Object>>();
 		for(int i = 0; i < 2; i++){
 			Map mm = getMap()
 					.put("id", "id-" + i)
 					.put("name", "name-" + i)
 					.build();
-			li.add(0, mm);	 
+			li.add(0, mm);
 		}
 		Tools.out(li);
 		li = turnListMap(li);
 		Tools.out(li);
-		
-		 
-		
-		
+
+
+
+
 	}
-	
+
 	public static int getMapSize(Map map){
 		return map.keySet().size();
 	}
-	
-	
-	
+
+
+
 	/**
 	 * 获取Map工厂build模式
 	 * @return
@@ -112,7 +114,7 @@ public class MapListUtil {
 	}
 	public static MakeLinkMap getLinkMap(){
 		return new MakeLinkMap();
-	} 
+	}
 	/**
 	 * 获取ArrayList
 	 * @return
@@ -141,9 +143,9 @@ public class MapListUtil {
 			}
 		}
 		return -1;
-		
+
 	}
-	
+
 	/**
 	 * List<Map> 转换为 List<Map<String, String>
 	 * @param list
@@ -159,7 +161,7 @@ public class MapListUtil {
 		}
 		return res;
 	}
-	
+
 	/**
 	 * 获取list<map>的第i行name列
 	 */
@@ -173,48 +175,41 @@ public class MapListUtil {
 		if(list.get(i).get(name) == null){
 			return defaultValue;
 		}else{
-			return list.get(i).get(name).toString();
+			return getMap(list.get(i), "name");
 		}
 	}
-	/**
-	 * 获取map的name列
-	 * @param map
-	 * @param name
-	 * @return
-	 */
 	public static String getMap(Map map, String name){
 		return getMap(map, name, "");
 	}
 
-	/**
-	 * 获取map的name列
-	 * @param map
-	 * @param name
-	 * @return
-	 */
-	public static String getMap(Map map, String name, String defaultValue){
-		if(map == null)return "";
-		if(map.get(name) == null){
-			return defaultValue;
+	public static <T> T getMap(Map map, Object key, T defaultValue){
+		Object obj = map.get(key);
+		T res = null;
+		if(obj == null)obj = map.get(key.toString().toLowerCase());
+		if(obj == null)obj = map.get(key.toString().toUpperCase());
+
+		if(obj == null) {
+			res = defaultValue;
 		}else{
-			return map.get(name).toString();
+			if (defaultValue instanceof String) {
+				res = (T)(obj.toString());
+			} else if (obj instanceof String) {
+				if (defaultValue instanceof Integer) {
+					res = (T)(new Integer(Tools.parseInt(obj.toString())));
+				} else if (defaultValue instanceof Double) {
+					res = (T)(new Double(Tools.parseDouble(obj.toString())));
+				}else{
+					res = (T)obj;
+				}
+			}else{
+				res = (T)obj;
+			}
 		}
+
+		return res;
 	}
-	/**
-	 * 获取map的name列
-	 * @param map
-	 * @param name
-	 * @return Object
-	 */
-	public static Object getMap(Map map, String name, Object defaultValue){
-		if(map == null)return "map is null";
-		if(map.get(name) == null){
-			return defaultValue;
-		}else{
-			return map.get(name);
-		}
-	}
-	
+
+
 	/**
 	 * 转换Map<String, Object> -> Map<String, Double>
 	 * @param map
@@ -279,45 +274,45 @@ public class MapListUtil {
 				for( int i = 0;i < list2.size(); i++){
 					list1.add(list2.get(i));
 				}
-			} 
+			}
 		}else{
 			list1 = list2;
 		}
-		
+
 		return list1;
 	}
-	
+
 	public static List<List<String>> toArrayAndTurn(List<Map<String, Object>> list){
 		return turnListString(toArray(list));
 	}
-	
+
 	public static List<List<String>> toArray(List<Map<String, Object>> list){
 		List<List<String>> res = new ArrayList<List<String>>();
-		
+
 		if(list != null && list.size() > 0){
-			Set set = list.get(0).keySet(); 
-			int colSize = set.size();	
-			int rowSize = list.size(); 
-			
+			Set set = list.get(0).keySet();
+			int colSize = set.size();
+			int rowSize = list.size();
+
 			for(int i = 0; i < rowSize; i++){
 				List<String> ll = new ArrayList<String>();
 				for (Object key : set) {
 					ll.add(getList(list, i, ""+key));
 				}
 				res.add(ll);
-			} 
+			}
 		}
-		
+
 		return res;
 	}
 	public static List<List<String>> turnListString(List<List<String>> list){
-		
+
 		List<List<String>> res = new ArrayList<List<String>>();
-		
-		if(list != null && list.size() > 0){ 
-			int colSize = list.get(0).size();	
+
+		if(list != null && list.size() > 0){
+			int colSize = list.get(0).size();
 			int rowSize = list.size();
-			for(int i = 0; i < colSize; i++){ 
+			for(int i = 0; i < colSize; i++){
 				List<String> ll = new ArrayList<String>();
 				for(int j = 0; j < rowSize; j++){
 					ll.add( list.get(j).get(i));
@@ -325,12 +320,12 @@ public class MapListUtil {
 				res.add(ll);
 			}
 		}
-		
+
 		return res;
 	}
 	/**
 	 * List<Map> 行列转换
-	 * row1: col11, col12, col13 
+	 * row1: col11, col12, col13
 	 * row2: col21, col22, col23
 	 * row3: col31, col32, col33
 	 * row4: col41, col42, col43
@@ -340,12 +335,12 @@ public class MapListUtil {
 	 * 		col13, col23, col33, col43
 	 */
 	public static List<Map<String, Object>> turnListMap(List<Map<String, Object>> list){
-		
+
 		List<Map<String, Object>> res = new ArrayList<Map<String, Object>>();
-		
+
 		if(list != null && list.size() > 0){
-			Set set = list.get(0).keySet(); 
-			int colSize = set.size();	
+			Set set = list.get(0).keySet();
+			int colSize = set.size();
 			int rowSize = list.size();
 			int cc = 0;
 			for (Object key : set) {
@@ -357,65 +352,65 @@ public class MapListUtil {
 				res.add(0, col);
 				cc++;
 			}
-			
+
 		}
-		
+
 		return res;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public static List<Map> testList(){
 		List<Map> res = new ArrayList<Map>();
-		
+
 		for(int i = 0; i < 4; i++){
 			res.add(
 					map()
-					.put("id", i)
-					.put("name", "name-" + i)
-					.build()
-					);
+							.put("id", i)
+							.put("name", "name-" + i)
+							.build()
+			);
 		}
-		
+
 		return res;
 	}
-	
+
 	public static Map<String, String> testSSMap(){
 		Map<String, String> res = new LinkedHashMap<>();
-		for(int i = 0; i < 3; i++){ 
-			res.put("id", "id" + i); 
-			res.put("name", "name" + i); 
+		for(int i = 0; i < 3; i++){
+			res.put("id", "id" + i);
+			res.put("name", "name" + i);
 		}
-		return res;		
+		return res;
 	}
 	public static Map<String, Object> testSOMap(){
 		Map<String, Object> res = new LinkedHashMap<>();
-		for(int i = 0; i < 3; i++){ 
-			res.put("id", "id" + i); 
-			res.put("name", "name" + i); 
+		for(int i = 0; i < 3; i++){
+			res.put("id", "id" + i);
+			res.put("name", "name" + i);
 		}
-		return res;		
+		return res;
 	}
 	public static Map<String, Double> testSDMap(){
 		Map<String, Double> res = new LinkedHashMap<>();
-		for(int i = 0; i < 3; i++){ 
-			res.put("id",  i * 1.0); 
-			res.put("name", i * 2.0); 
+		for(int i = 0; i < 3; i++){
+			res.put("id",  i * 1.0);
+			res.put("name", i * 2.0);
 		}
-		return res;		
+		return res;
 	}
-	 
+
 }
