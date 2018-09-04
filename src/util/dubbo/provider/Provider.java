6ed7f5d117f286d1d170911cd3be046f5cd521bf
Walker <1424234500@qq.com>
2018-09-04 17:24:15 +0800

    Tue Sep  4 17:24:15 DST 2018

--- src/util/dubbo/provider/Provider.java
+++ src/util/dubbo/provider/Provider.java
@@ -7,11 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  */
 public class Provider {
-    public static void main(String[] args) throws Exception {
-        System.setProperty("java.net.preferIPv4Stack", "true");
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-provider.xml"});
-        context.start();
-        System.out.println("Provider started.");
-        System.in.read(); // press any key to exit
-    }
+	public static void main(String[] args) throws Exception {
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "dubbo-provider.xml" });
+		context.start();
+		System.out.println("Provider started.");
+		System.in.read(); // press any key to exit
+	}
 }
