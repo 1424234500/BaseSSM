e082dc98f990506c3968526ba4b2c983cba9d8c0
Walker <1424234500@qq.com>
2018-10-21 14:23:38 +0800

    Sun Oct 21 14:23:38 DST 2018

--- src/util/ThreadUtil.java
+++ src/util/ThreadUtil.java
@@ -13,6 +13,11 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.junit.Test;
 
 import util.cache.CacheMgr;
 import util.setting.Setting;
@@ -31,9 +36,9 @@ public class ThreadUtil {
 	public static final int ScheduledThread = 3;//定时任务线程池
 
 	//存储三种池 统一
-	private static Map<Integer, ExecutorService>          mapExec = new HashMap<>();
+	private static final Map<Integer, ExecutorService>          mapExec;
 	static{
-		
+		mapExec = new HashMap<>();
 	}
 //	private static ScheduledExecutorService 			  scheduleExec;
 
@@ -390,4 +395,99 @@ public class ThreadUtil {
 			System.out.println("Thread.sleep error " + time);
 		}
 	}
+	
+	static int iSignal = 0;
+
+	/**
+	 * 测试线程相关
+	 */
+	@Test
+	public void ttt( ){
+		ThreadUtil.execute(new Runnable() {
+			@Override
+			public void run() {
+				int i = 0;
+				while(! Thread.interrupted()){
+					System.out.println("execute:" + i);
+					i+=2;
+					Thread.yield();
+					sleep(1000);
+				}
+			}
+		});
+		final Lock lock = new ReentrantLock();
+		final Condition condition = lock.newCondition(); //依赖于锁lock的信号标识量
+		ThreadUtil.execute(new Runnable() {
+			@Override
+			public void run() {
+				while(! Thread.interrupted()){
+					if(iSignal % 3 == 1){
+						lock.lock();
+						try{
+							System.out.println("execute 1:" + iSignal);
+							iSignal++;
+							sleep(1000);
+							condition.notifyAll();
+						}finally{
+							lock.unlock();
+						}
+					}else{
+						try {
+							condition.await();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		Future<?> f = ThreadUtil.submit(DefaultThread, new Runnable() {
+			@Override
+			public void run() {
+				while(! Thread.interrupted()){
+					if(iSignal % 3 == 2){
+						lock.lock();
+						try{
+							System.out.println("execute 2:" + iSignal);
+							iSignal++;
+							sleep(1000);
+							condition.notifyAll();
+						}finally{
+							lock.unlock();
+						}
+					}else{
+						try {
+							condition.await(); //等待condition条件信号
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		f.cancel(true);
+		
+//		try {
+//			wait(); //非同步控制方法里调用  0.可以由notify恢复 1.释放锁  所以异常 调用时必须要获得锁才能?    sleep不释放锁  不可恢复 只是暂时让出cpu
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		
+		Object lockObj = new Object();
+		synchronized (lockObj) {
+			lockObj.notifyAll();//唤醒所有持有该锁?
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
